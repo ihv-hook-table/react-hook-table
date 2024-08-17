452384e7ff16dade654d8842ea4f094eb1b0d6cf
toonmeister <76071246+toonmeister@users.noreply.github.com>
2024-08-17 09:36:59 +0300

    remove log

--- src/hook-table/useTable.tsx
+++ src/hook-table/useTable.tsx
@@ -2,7 +2,6 @@ import { TableRowType } from './types';
 import { useCreateColumn, useCreateTable } from './components';
 
 export const useTable = <T extends TableRowType = TableRowType>() => {
-  console.log(navigator.language);
   const { Table } = useCreateTable<T>();
   const { Column } = useCreateColumn<T>();
 
