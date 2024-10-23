fdacfeb4f33933137908acb136d7fad6907af180
toonmeister <76071246+toonmeister@users.noreply.github.com>
2024-10-23 08:08:25 +0300

    remove expandable

--- src/hook-table/components/Expandable/Expandable.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { ExpandableRowProps, TableRecord } from '../../types';
-
-export const Expandable = <T extends TableRecord = TableRecord>(
-  _props: ExpandableRowProps<T>,
-): JSX.Element => {
-  return <></>;
-};
